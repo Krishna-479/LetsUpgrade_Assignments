6384ac608faf7e11ceb496455c610918810d5896
Krishna-479 <91129933+Krishna-479@users.noreply.github.com>
2021-09-21 17:59:23 +0530

    Day_1 Assignment

---
+++ Day1_Assignment.py
@@ -0,0 +1,5 @@
+P=float(input("Enter the Principle(in Rs.)= "))
+T=int(input("Enter the Time(in years)= "))
+R=float(input("Enter the rate= "))
+SimpleInterest =(P*T*R)/100;
+print("Simple Interest is",SimpleInterest)
